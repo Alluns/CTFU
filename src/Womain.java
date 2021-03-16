54ee4ecb5942d0a10c74c75c80f2eed8d2d2030b
Alluns <Allan.Backman@gmail.com>
2021-03-16 13:26:19 +0100

    initial commit

---
+++ src/Womain.java
@@ -0,0 +1,12 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Womain {
+    //Womain is the server, please be respectful
+    public static void main(String[] args) {
+        int port = 1234;
+        ServerSocket serverSocket;
+        Socket socket;
+
+    }
+}
